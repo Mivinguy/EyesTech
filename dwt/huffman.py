10692946f5cc6834c280e43735d6535b2d1ad3dd
Michael Nguyen <mivinguy@ucsc.edu>
2020-11-25 16:49:29 -0800

    Continued development of Huffman coding

--- dwt/huffman.py
+++ dwt/huffman.py
@@ -33,6 +33,7 @@ class huffmanCoding:
         self.image = image
         self.heap = []
         self.codeBook = {}
+        self.bitBuffer = []
 
     def frequency(self, image):
         freq = {}
@@ -49,7 +50,7 @@ class huffmanCoding:
             node = Node(val, frequency[val])
             heapq.heappush(self.heap, node)
     
-    def buildNodes(self):
+    def buildTree(self):
         while(len(self.heap)>1):
             node1 = heapq.heappop(self.heap)
             node2 = heapq.heappop(self.heap)
@@ -68,7 +69,6 @@ class huffmanCoding:
     def buildCodebookHelper(self, root, currentCode):
         if(root == None):
             return
-
         if(root.val != None):
             self.codeBook[root.val] = currentCode
             return
@@ -76,20 +76,39 @@ class huffmanCoding:
         self.buildCodebookHelper(root.left, currentCode + "0")
         self.buildCodebookHelper(root.right, currentCode + "1")
 
-    def encodeImage(self, image):
-        encodedImage =  ""
+    def encodeImage(self, image, stream):
         for row in range(np.shape(image)[0]):
             for col in range(np.shape(image)[1]):
-                encodedImage += self.codeBook[(image[row][col])]
-        return encodedImage
+                currentCode = self.codeBook[(image[row][col])]
+                self.bitsToBuffer(currentCode, stream)
 
+    def bitsToBuffer(self, bitStr, stream):
+        if bitStr is not None:
+            for bit in range(len(bitStr)):
+                self.bitBuffer.append(bitStr[bit])
+                if len(self.bitBuffer) >= 8:
+                    self.bufferToFile(stream)
+    
+    def bufferToFile(self, stream):
+        byte = self.bitBuffer[:8]
+        self.bitBuffer = self.bitBuffer[8:]
+        str = ""
+        intValue = int(str.join(byte), 2)
+        stream.write(bytes(intValue))
 
     def compress(self):
+        fileNameW = 'compressedFrame.bin'
+        outFile = open(fileNameW, 'wb')
         freq = self.frequency(self.image)
         self.createHeap(freq)
-        self.buildNodes()
+        self.buildTree()
         self.buildCodebook()
-        encodedImage = self.encodeImage(self.image)
+        print(len(self.codeBook))
+        encodedImage = self.encodeImage(self.image, outFile)
+        self.bitsToBuffer(encodedImage, outFile)
+        #fileNameW = 'compressedFrame.bin'
+        #outFile = open(fileNameW, 'wb')
+        #outFile.write(bytes(b))
         #print(self.codeBook)
 
 """ Testing """
@@ -109,3 +128,4 @@ image_stream.seek(0)
 original = np.frombuffer(image_stream.getvalue(), dtype=np.uint8).reshape(318,183) # reshaped the test image, it is originally of dimension (58194,)
 huffmanCoding(original).compress()
 print('input image size: ', original.size)
+
